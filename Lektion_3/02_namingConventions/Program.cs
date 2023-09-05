69dc00f0d24720a5e74bcc49e62eb1c7ee52e9a1
AnnelieP1 <annelie.pettersson@yh.nackademin.se>
2023-09-05 13:49:16 +0200

    genomgång 3 lektion 3

---
+++ Lektion_3/02_namingConventions/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+
+Design Pattern vid Namngivning
+_____________________________________________________
+
+camelCase           firstName       Javascript, Java C/C++ (C#)
+Pascal              FirstName       C#, Pascal, Turbo 
+snake_case          first_name      Python, C/C++
+kebab-case          first-name      powerShell, CSS
+
+Variabler           string firstName            string firstName
+Privata variabler   string _firstName           private string _firstName
+Parameter           string firstName            MehtodName(string firstName, string lastName) {}
+
+Class - fields      firstName                   private string _firstName;
+Class - Properties  firstName                   public string FirstName {get;set;}
+Class - Methods     MethodName                  public void MethodName () {}
+
+
+______________________________________________________________________________________________________________________________________
+
+HANS GENOMGÅNG
+______________________________________________________________________________________________________________________________________
+﻿/*  
+ 
+    DESIGN PATTERN vid Namngivning
+    ------------------------------------------------------------------------------------
+    
+    camelCase               firstName           Javascript, Java, C/C++, (C#)
+    Pascal                  FirstName           C#, Pascal, Turbo Pascal
+    snake_case              first_name          Python, C/C++
+    kebab-case              first-name          PowerShell, CSS
+ 
+    Variabler               firstName           string firstName
+    Privata variabler       _firstName          private string _firstName
+    Parameter               firstName           MehtodName(string firstName, string lastName) {}
+ 
+    Class - fields          firstName           private string _firstName;
+    Class - Properties      FirstName           public string FirstName { get;set; }
+    Class - Methods         MethodName          public void MethodName() {}
+
+*/
+
+
